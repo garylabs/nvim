f3c69099f4565dafa88466f6360a4524b492f184
Gurveer Singh <65100988+garylabs@users.noreply.github.com>
2024-12-31 02:19:24 -0500

    Update colorscheme.lua

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,6 +1,28 @@
 return {
   'rebelot/kanagawa.nvim',
   config = function()
+    require('kanagawa').setup {
+      transparent = false,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = 'none',
+            },
+          },
+        },
+      },
+    }
+
+    local bg_transparent = true
+    local toggle_transparency = function()
+      bg_transparent = not bg_transparent
+      require('kanagawa').setup {
+        transparent = bg_transparent,
+      }
+      vim.cmd.colorscheme 'kanagawa'
+    end
+    vim.keymap.set('n', '<leader>bg', toggle_transparency, { desc = 'Toggle Transparency' })
     vim.cmd.colorscheme 'kanagawa'
   end,
 }
